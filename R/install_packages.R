f5ef82304c0feb30ed6bfa67d6dba65a029c852f
Kyle M. Lang <kylelang86@gmail.com>
2024-08-20 13:20:11 +0200

    messing with the directory structure

--- R/install_packages.R
+++ R/install_packages.R
@@ -2,5 +2,6 @@ renv::status()
 renv::restore(exclude = "sf", repos = "https://cloud.r-project.org")
 renv::update()
 renv::install("maps", repos = "https://cloud.r-project.org")
+renv::snapshot()
 
 install.packages("sf", repos = "https://cloud.r-project.org")
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-REMOTE_HOST="ath-cloud"
-REMOTE_DIR="~/sites/r-primers.andrewheiss.com/public"
-REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
-
-echo "Uploading new changes to remote server..."
-echo
-rsync -crvP --delete _site/ $REMOTE_DEST
